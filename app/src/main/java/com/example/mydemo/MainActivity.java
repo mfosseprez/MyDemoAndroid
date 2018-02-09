dae230efea215636c014757cba629f17a41fc4ca
mfosseprez <marc.fosseprez@gmail.com>
2018-02-09 20:36:34 +0100

    First commit

---
+++ app/src/main/java/com/example/mydemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
